0b8315133b308df4893bdacbb825d22997c81a96
Kayla Patel <paylakatel@gmail.com>
2018-03-18 00:10:01 -0400

    gatsby initial commit

---
+++ site/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div className="tr ph3 ph7-l ph6-m">
+    <h1 className="f1 lh-title f-headline-l">Welcome<span className="accent">!</span></h1>
+    <p className="f4 f3-l lh-copy">I’m Kayla, and I build solutions that help governments and organizations better serve their users.
+    <br /><br />My academic background is in geospatial science, and my career: public sector tech.</p>
+  </div>
+)
+
+export default IndexPage
