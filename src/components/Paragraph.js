16187bce1a4f81a3478bd5c1bcde657a1ce2c8b8
Kayla Patel <paylakatel@gmail.com>
2018-03-20 08:55:25 -0400

    adds better spacing on desktop

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -5,7 +5,7 @@ import siteTheme from "./siteTheme"
 
 // we create styled paragraph element with alignRight property
 const Paragraph = styled.p.attrs({
-    className: "pa2 mh4 f4 f3git-ns mh6-ns",
+    className: "pa2 mh4 f4 ml7-l pl7-l f3-ns mh6-ns",
 })`
     @font-face {
         font-family: ${props => props.theme.paragraphFont};
