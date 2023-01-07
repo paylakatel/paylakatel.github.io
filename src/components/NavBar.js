9bdcc1fe18b0084a8b968488ac1634e4820833c6
Kayla Patel <paylakatel@gmail.com>
2023-01-07 14:46:32 -0500

    remove res, update position

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,44 +1,29 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import './global.css';
-import { StaticQuery, graphql } from 'gatsby';
 
 const NavBar = () => (
-  <StaticQuery
-    query={graphql`
-      query ResumeQuery {
-        resume: file(relativePath: { eq: "KPatel_Resume.pdf" }) {
-          publicURL
-        }
-      }
-    `}
-    render={data => (
-      <nav>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          <h3 className="site-title">
-            <Link to="/">Kayla A. Patel</Link>
-          </h3>
+  <nav>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+    >
+      <h3 className="site-title">
+        <Link to="/">Kayla A. Patel</Link>
+      </h3>
 
-          <ul className="navBar">
-            <li>
-              <Link to="/">About</Link>
-            </li>{' '}
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>{' '}
-            <li>
-              <a href={data.resume.publicURL}>Resume</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    )}
-  />
+      <ul className="navBar">
+        <li>
+          <Link to="/">About</Link>
+        </li>{' '}
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>{' '}
+      </ul>
+    </div>
+  </nav>
 );
 
 export default NavBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, StaticQuery, graphql } from 'gatsby';
+import { Link } from 'gatsby';
 
 import IndexLayout from '../components/IndexLayout';
 import { Mail, Twitter, GitHub } from 'react-feather';
@@ -11,71 +11,51 @@ import PageHead from '../components/PageHead';
 export const Head = () => <PageHead />;
 
 const IndexPage = () => (
-  <StaticQuery
-    query={graphql`
-      query IndexResumeQuery {
-        resume: file(relativePath: { eq: "KPatel_Resume.pdf" }) {
-          publicURL
-        }
-      }
-    `}
-    render={data => (
-      <IndexLayout>
-        <h1>
-          Welcome
-          <span className="punctuation">!</span>
-        </h1>
-        <div className="intro">
-          <p>
-            I’m Kayla, and I build solutions that help governments and
-            organizations better serve their users.
-          </p>
-          <p>
-            My academic background is in geospatial science, and my career:
-            public sector tech.
-          </p>
-          <p>
-            Currently, I work for the City of Boston, MA as a Product Manager on
-            our{' '}
-            <a href="https://www.boston.gov/departments/digital-team">
-              Digital Team
-            </a>
-            .
-          </p>
-        </div>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-          }}
-        >
-          <ul className="navBar">
-            <li>
-              <Link to="/projects">
-                <h4>Projects</h4>
-              </Link>
-            </li>{' '}
-            <li>
-              <a href={data.resume.publicURL}>
-                <h4>Resume</h4>
-              </a>
-            </li>
-          </ul>{' '}
-        </div>
-        <div className="icons">
-          <a className="icon" href="mailto:paylakatel@gmail.com">
-            <Mail />
-          </a>
-          <a className="icon" href="https://twitter.com/paylakatel">
-            <Twitter />
-          </a>
-          <a className="icon" href="https://github.com/paylakatel">
-            <GitHub />
-          </a>
-        </div>
-      </IndexLayout>
-    )}
-  />
+  <IndexLayout>
+    <h1>
+      Welcome
+      <span className="punctuation">!</span>
+    </h1>
+    <div className="intro">
+      <p>
+        I’m Kayla, and I build solutions that help governments and organizations
+        better serve their users.
+      </p>
+      <p>
+        My academic background is in geospatial science, and my career: public
+        sector tech.
+      </p>
+      <p>
+        Currently, I work for Hi Marley in Boston, MA as a Senior Software
+        Engineer.
+      </p>
+    </div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+      }}
+    >
+      <ul className="navBar">
+        <li>
+          <Link to="/projects">
+            <h4>Projects</h4>
+          </Link>
+        </li>{' '}
+      </ul>{' '}
+    </div>
+    <div className="icons">
+      <a className="icon" href="mailto:paylakatel@gmail.com">
+        <Mail />
+      </a>
+      <a className="icon" href="https://twitter.com/paylakatel">
+        <Twitter />
+      </a>
+      <a className="icon" href="https://github.com/paylakatel">
+        <GitHub />
+      </a>
+    </div>
+  </IndexLayout>
 );
 
 export default IndexPage;
